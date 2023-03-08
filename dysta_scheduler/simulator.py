556590846c3a89decc1c64465624c279071c8e2a
s.venieris <s.venieris@samsung.com>
2023-03-08 11:30:34 +0000

    Brought CLI args back to simulator.py.

--- dysta_scheduler/simulator.py
+++ dysta_scheduler/simulator.py
@@ -1,10 +1,9 @@
 from scheduler import FCFS_Scheduler, Dysta_Scheduler, PREMA_Scheduler
 from utils import generate_reqst_table, construct_lat_table
-from options import args
-import random
+import sys
+import argparse
 import logging
-
-random.seed(args.seed)
+import random
 
 scheduler_dict = {"fcfs": FCFS_Scheduler,
                   "dysta": Dysta_Scheduler,
@@ -41,4 +40,45 @@ def simulation(args):
     print(f"Average Normalised Turnaround Time (ANTT): {antt:.2f}")
     
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description="Simulator for scheduling sparse multi-DNN workloads on sparse DNN accelerators.")
+
+  # Simulation configuration
+  parser.add_argument("--models", nargs='+', default="bert", type=str, choices=["bert", "bart"],
+                      help="The name(s) of candidate models.")
+  parser.add_argument("--csv_lat_files", nargs='+', default="bert_lat.csv", type=str,
+                      help="The measured latencies of the supplied model(s) on the target accelerator.")
+  parser.add_argument("--schedule_method", nargs='+', default="fcfs", type=str, choices=["fcfs", "dysta", "prema"],
+                      help="The name(s) of the evaluated scheduling method(s).")
+  parser.add_argument("--sample_per_sec", default=30, type=int, 
+                      help="The input arrival rate in samples (or tasks) per second.")
+  parser.add_argument("--seq_len", default=512, type=int, required=False,
+                      help="The input sequence length for Transformer models.")
+
+  # Verbosity / Logging
+  parser.add_argument("--debug", action="store_true")
+
+  # Random seed
+  parser.add_argument("--seed", type=int, default=1,
+                      help="Random seed.")
+
+  # parser.add_argument("--dataset_root", default="/path/to/your/directory", type=str, help="The path to your dataset root")
+  # parser.add_argument("--figs_path", default="/path/to/your/directory", type=str, help="The path to all saved figures/images")
+  # parser.add_argument("--w_sparsity", default=0.95, type=float, help="Sparity of weight")
+
+  args = parser.parse_args()
+  if type(args.models) is not list:
+      args.models = [args.models]
+  if type(args.csv_lat_files) is not list:
+      args.csv_lat_files = [args.csv_lat_files]
+  if type(args.schedule_method) is not list:
+      args.schedule_method = [args.schedule_method]
+
+  # Logging setup
+  if args.debug:
+      logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+  else:
+      logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+  random.seed(args.seed)
+
   simulation(args)
